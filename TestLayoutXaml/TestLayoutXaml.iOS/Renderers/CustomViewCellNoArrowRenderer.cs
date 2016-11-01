2722511c79ec62d5082ff1376d613a391d3f36a8
Hrafn Loftsson <hrafn@kolibri.is>
2016-11-01 13:57:35 +0000

    Changes a StackLayout to a ContentView and adds a hack in comments to fix the spinner problem on iOS

--- TestLayoutXaml/TestLayoutXaml.iOS/Renderers/CustomViewCellNoArrowRenderer.cs
+++ TestLayoutXaml/TestLayoutXaml.iOS/Renderers/CustomViewCellNoArrowRenderer.cs
@@ -15,6 +15,24 @@ namespace TestLayout.iOS.Renderers
     {
         public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
         {
+			// The following fixes the spinner problem
+			// ======
+			/*
+			 var viewCell = item as ViewCell;
+			var view = viewCell.View;
+			if (view is ContentView)
+			{
+				var contentView = view as ContentView;
+				if (contentView.Content is ActivityIndicator)
+				{
+					var spinner = (ActivityIndicator)contentView.Content;
+					spinner.IsRunning = false;
+					spinner.IsRunning = true;
+				}
+			}
+			*/
+			// ======
+
             var cell = base.GetCell(item, reusableCell, tv);
 
             // remove highlight on selected cell
