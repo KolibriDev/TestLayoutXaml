870a14fde56703de60d5a563cdcde756393b8268
Hrafn Loftsson <hrafn@kolibri.is>
2017-08-14 09:26:46 +0000

    Adds missing files from last commit

---
+++ TestLayoutXaml/TestLayoutXaml.iOS/Renderers/CustomHeaderViewCellRenderer.cs
@@ -0,0 +1,26 @@
+﻿using TestLayout.iOS.Renderers;
+using TestLayoutXaml;
+using UIKit;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(HeaderViewCell), typeof(CustomHeaderViewCellRenderer))]
+
+
+namespace TestLayout.iOS.Renderers
+{
+    
+    public class CustomHeaderViewCellRenderer : ViewCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+
+            // remove highlight on selected cell
+            cell.SelectionStyle = UITableViewCellSelectionStyle.None;
+
+            return cell;
+        }
+    }
+}
---
+++ TestLayoutXaml/TestLayoutXaml/ListGroup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TestLayoutXaml
+{
+    public class ListGroup<TKey, T> : List<T>
+    {
+        public ListGroup(IGrouping<TKey, T> group)
+            : base(group)
+        {
+            this.Key = group.Key;
+        }
+
+        /// <summary>
+        /// Gets or sets the key for the group.
+        /// </summary>
+        public TKey Key
+        {
+            get; set;
+        }
+    }
+}
